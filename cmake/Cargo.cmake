bc11abbd39cbdcfabfcd2da9963166d5770636ee
Andrew Gaspar <andrew.gaspar@outlook.com>
2019-03-03 16:18:37 -0700

    Automatically detect GNU builds on Windows

--- cmake/Cargo.cmake
+++ cmake/Cargo.cmake
@@ -1,15 +1,36 @@
 option(CARGO_DEV_MODE OFF "Only for use when making changes to cmake-cargo.")
 
-if (CMAKE_VS_PLATFORM_NAME)
-    if ("${CMAKE_VS_PLATFORM_NAME}" STREQUAL "Win32")
-        set(CARGO_TARGET i686-pc-windows-msvc CACHE STRING
-            "The target triple to build for")
-    elseif("${CMAKE_VS_PLATFORM_NAME}" STREQUAL "x64")
-        set(CARGO_TARGET x86_64-pc-windows-msvc CACHE STRING
-            "The target triple to build for")
-    else()
-        message(WARNING "VS Platform '${CMAKE_VS_PLATFORM_NAME}' not recognized")
+if (WIN32)
+    if (CMAKE_VS_PLATFORM_NAME)
+        if ("${CMAKE_VS_PLATFORM_NAME}" STREQUAL "Win32")
+            set(CARGO_ARCH i686 CACHE STRING "Build for 32-bit x86")
+        elseif("${CMAKE_VS_PLATFORM_NAME}" STREQUAL "x64")
+            set(CARGO_ARCH x86_64 CACHE STRING "Build for 64-bit x86")
+        elseif("${CMAKE_VS_PLATFORM_NAME}" STREQUAL "ARM64")
+            set(CARGO_ARCH aarch64 CACHE STRING "Build for 64-bit ARM")
+        else()
+            message(WARNING "VS Platform '${CMAKE_VS_PLATFORM_NAME}' not recognized")
+        endif()
+    else ()
+        if (CMAKE_SIZEOF_VOID_P EQUAL 8)
+            set(CARGO_ARCH x86_64 CACHE STRING "Build for 64-bit x86")
+        else()
+            set(CARGO_ARCH i686 CACHE STRING "Build for 32-bit x86")
+        endif()
     endif()
+
+    set(CARGO_VENDOR "pc-windows" CACHE STRING "Build for Microsoft Windows")
+
+    message(STATUS "${CMAKE_CXX_COMPILER_ID}")
+
+    if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+        set(CARGO_ABI gnu CACHE STRING "Build for linking with GNU")
+    else()
+        set(CARGO_ABI msvc CACHE STRING "Build for linking with MSVC")
+    endif()
+
+    set(CARGO_TARGET "${CARGO_ARCH}-${CARGO_VENDOR}-${CARGO_ABI}" CACHE STRING
+        "Cargo target triple")
 endif()
     
 if (CARGO_DEV_MODE)
