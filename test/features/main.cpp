dd2cf712c60e3f4d4bc30aaa41882ceae06cf92e
Simon Hausmann <hausmann@gmail.com>
2021-09-02 18:13:01 +0200

    Fix the multi-feature test
    Reference the second function that's guarded by the second feature, otherwise we don't
    know if both were passed.

--- test/features/main.cpp
+++ test/features/main.cpp
@@ -1,8 +1,10 @@
 extern "C" void rust_function(char const *name);
+extern "C" void rust_second_function(char const *name);
 
 int main(int argc, char **argv) {
     if (argc < 2) {
         rust_function("C++");
+        rust_second_function("C++ again");
     } else {
         rust_function(argv[1]);
     }
