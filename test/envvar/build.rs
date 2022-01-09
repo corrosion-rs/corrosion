5ff70498ae78be1116ec4ae2c3cca1e0f865e67f
Jyun-Yan You <jyyou.tw@gmail.com>
2022-01-09 12:25:46 +0800

    Add test for LINKER_LANGUAGE

--- test/envvar/build.rs
+++ test/envvar/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    assert!(!env!("CORROSION_LINKER_LANGUAGES").is_empty());
     assert_eq!(env!("REQUIRED_VARIABLE"), "EXPECTED_VALUE");
     assert_eq!(std::env::var("ANOTHER_VARIABLE").unwrap(), "ANOTHER_VALUE");
 }
