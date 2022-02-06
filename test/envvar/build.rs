0b0b182f8d2e12a1558d12dc5aea15bc815c703b
Jonathan Schwender <schwenderjonathan@gmail.com>
2022-02-06 12:00:19 +0100

    Set CC and CXX for cargo
    Cargo build scripts may themselves build c-dependencies and may choose
    the compiler by inspecting CC or CXX.
    Thus we should pass the compiler that was set in CMake, or alternatively
    that is set in the environment to cargo.
    This allows cargo to use the same compiler.
    
    Signed-off-by: Jonathan Schwender <schwenderjonathan@gmail.com>

--- test/envvar/build.rs
+++ test/envvar/build.rs
@@ -1,4 +1,5 @@
 fn main() {
     assert_eq!(env!("REQUIRED_VARIABLE"), "EXPECTED_VALUE");
     assert_eq!(std::env::var("ANOTHER_VARIABLE").unwrap(), "ANOTHER_VALUE");
+    assert_eq!(env!("CC"), "some-custom-c-compiler");
 }
