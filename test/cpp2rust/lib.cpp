d1d3346a2105ba16df4c47f1bd1808c0ac8eae3b
Stephan Guilloux (cos) <stephan.guilloux@crisalid.com>
2022-05-19 14:26:36 +0200

    Corrosion tests should not test C++ features not used by Corrosion itself.
    This PR follows discussion for #183.
    
    * Problem addressed with this PR:
    - CMakeLists.txt uses std_cxx_17 to compile it's test cases.
    - Tests cases need std_cxx_17, because of their use of <string_view>.
    Because of both, Corrosion fails to compile on 'old' systems (I did not say vintage :-)).
    
    * Investigations:
    As Corrosion does not use <string_view>, it's not needed to use this in its test cases.
    If <string_view> is not used anywhere, options std_cxx_17 are no more required.
    
    * Correction proposal:
    Use of std_cxx_14, instead of std_cxx_17.
    Remove the use of <string_view> in Corrosion test cases.
    Altogether, this allows older compilers to build Corrosion.
    
    * Remark:
    This PR keeps some std_cxx_14 (instead of _17) to keep the need of C++ Standard Library installed on the compilation host.
    I did not test, but I suppose std_cxx_11 would work too.

--- test/cpp2rust/lib.cpp
+++ test/cpp2rust/lib.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <string_view>
 
 extern "C" void cpp_function(char const *name) {
-    std::string_view const name_sv = name;
-    std::cout << "Hello, " << name_sv << "! I'm C++!\n";
+    std::cout << "Hello, " << "my name is lib.cpp" << "! I'm C++!\n";
 }
--- test/cpp2rust/path with space/lib3.cpp
+++ test/cpp2rust/path with space/lib3.cpp
@@ -1,10 +1,8 @@
 // Check that libraries located at a path containing a space can also be linked.
 
 #include <iostream>
-#include <string_view>
 
 extern "C" void cpp_function3(char const *name) {
-    std::string_view const name_sv = name;
-    std::cout << "Hello, " << name_sv << "! I'm C++ library Number 3!\n";
+    std::cout << "Hello, " << "my name is lib3.cpp" << "! I'm C++ library Number 3!\n";
 }
 
