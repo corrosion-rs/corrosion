e573f1b28b660c2e07d1f213e1f391fb1cce404c
Jonathan Schwender <jonathan.schwender@huawei.com>
2022-07-13 03:02:29 +0200

    Improve cpp2rust test
    Also test that rust dependencies link with foreign code

--- test/cpp2rust/lib2.cpp
+++ test/cpp2rust/lib2.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <stdint.h>
 
 extern "C" void cpp_function2(char const *name) {
     std::cout << "Hello, " << name << "! I'm C++ library Number 2!\n";
 }
 
+extern "C" uint32_t get_42() {
+    uint32_t v = 42;
+    return v;
+}
