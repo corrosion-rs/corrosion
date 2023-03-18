0553bbc005eb6ed9438db855a30667bfcc1e80d1
Jonathan Schwender <schwenderjonathan@gmail.com>
2023-03-18 14:57:31 +0100

    cxxbridge: Generate rust/cxx.h
    Previously we did not generate this header file,
    which caused some functionality to be missing.

--- test/cxxbridge/cxxbridge_cpp2rust/cpplib.cpp
+++ test/cxxbridge/cxxbridge_cpp2rust/cpplib.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "cpplib.h"
 #include "cxxbridge-cpp/lib.h"
-
-// todo: the cxx cli does not seem to generate this binding.
-//std::ostream &operator<<(std::ostream &, const rust::Str &);
+#include "rust/cxx.h"
 
 RsImage read_image(rust::Str path) {
-    //std::cout << path << std::endl;
+    std::cout << "read_image called" << std::endl;
+    std::cout << path << std::endl;
     Rgba c = { 1.0, 2.0, 3.0, 4.0};
     RsImage v = { 1, 1, c};
     return v;
