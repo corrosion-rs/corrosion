36bbcd8384487545b632bed3808a0547fcf849b4
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2024-06-22 21:11:09 +0200

    Fix typo
    Co-authored-by: kotoriのねこ <minamiktr@outlook.com>

--- cmake/FindRust.cmake
+++ cmake/FindRust.cmake
@@ -190,7 +190,7 @@ function(_corrosion_determine_libs_new target_triple out_libs out_flags)
             # We leave it up to the C/C++ executable that links in the Rust static-library
             # to determine which version of the msvc runtime library it should select.
             list(FILTER libs_list EXCLUDE REGEX "^msvcrtd?")
-            list(FILTER flags_list EXCLUDE REGEX "^/defaultlib:msvcrtd?")
+            list(FILTER flag_list EXCLUDE REGEX "^/defaultlib:msvcrtd?")
         else()
             message(DEBUG "Determining required native libraries - failed: Regex match failure.")
             message(DEBUG "`native-static-libs` not found in: `${cargo_build_error_message}`")
