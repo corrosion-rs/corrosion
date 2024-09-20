0cbe37f1b20128ed556104d03a70cfc5b4800787
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-09-20 19:23:34 +0200

    test msvc /link

--- cmake/Corrosion.cmake
+++ cmake/Corrosion.cmake
@@ -589,7 +589,7 @@ function(_add_cargo_build out_cargo_build_out_dir)
                 -P ${CORROSION_CMAKE_SRC_DIR}/CorrosionInvokeRustc.cmake
                 --)
         if(Rust_CARGO_TARGET_ENV STREQUAL "msvc")
-            target_link_libraries(${target_name} INTERFACE "@${native_libs_rsp_file}")
+            target_link_libraries(${target_name} INTERFACE "/link@${native_libs_rsp_file}")
         else()
             target_link_libraries(${target_name} INTERFACE "-Wl,@$<SHELL_PATH:${native_libs_rsp_file}>")
         endif()
