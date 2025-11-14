6021a0c357092bdbf581f5c0c7ce04678d1ede30
Jonathan Schwender <55576758+jschwe@users.noreply.github.com>
2025-11-14 08:42:04 +0100

    Initial support for iOS targets (#636)
    Add initial support for iOS targets. This works around two issues:
    1. When using XCode, `cc` points to a cross-compiler for the target platform, which makes build-scripts fail to link (i.e. the build-script-build fails). We fix this by determining a suitable compiler for the host target and telling cargo to use that.
    2. `$<TARGET_LINKER_FILE_DIR>` does not work properly when compiling for iOS - `EFFECTIVE_PLATFORM_NAME` leaks into the expanded generator expression, and is not expanded itself, causing the directory to be wrong. Annoyingly, `EFFECTIVE_PLATFORM_NAME=OFF` does not seem to fully prevent this issue (we should file an upstream bug), hence we need to manually determine the directory name based on the platform name and the build config. This is a hacky, best effort workaround, and should ideally be fixed upstream in CMake.
    
    Co-authored-by: Raphael Menges <Raphael.Menges@alfatraining.de>

--- cmake/Corrosion.cmake
+++ cmake/Corrosion.cmake
@@ -787,6 +787,28 @@ function(_add_cargo_build out_cargo_build_out_dir)
         list(APPEND corrosion_cc_rs_flags "AR_${stripped_target_triple}=${CMAKE_AR}")
     endif()
 
+    # Todo: use a cache variable to avoid rechecking
+    # When using XCode to target iOS / iOSSimulator, `cc` will be a compiler that targets iOS.
+    # (Presumably this is because XCode modifies PATH).
+    # This causes linker errors, because Rust compiles build-scripts and proc-macros for the host-platform, and
+    # assumes `cc` is a valid linker driver for the host platform (but in this case `cc` targets iOS).
+    # To work around this we explicitly set the linker for the host platform, and use a dummy CMake project
+    # to determine a suitable C compiler.
+    unset(cargo_host_target_linker)
+    if(CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin" AND CMAKE_SYSTEM_NAME STREQUAL "iOS")
+        message(CHECK_START "Determining linker for host architecture (${Rust_CARGO_HOST_TARGET_CACHED})")
+        string(TOUPPER Rust_CARGO_HOST_TARGET_CACHED host_target_upper)
+        string(REPLACE "-" "_" host_target_upper host_target_upper_underscore)
+        unset(corrosion_host_c_compiler)
+        _corrosion_determine_host_compiler(corrosion_host_c_compiler cor_error_info)
+        if(DEFINED corrosion_host_c_compiler)
+            message(CHECK_PASS "${corrosion_host_c_compiler}")
+            set(cargo_host_target_linker "CARGO_TARGET_${host_target_upper_underscore}_LINKER=${corrosion_host_c_compiler}")
+        else()
+            message(CHECK_FAIL "Failed - ${cor_error_info}")
+        endif()
+    endif()
+
     # Since we instruct cc-rs to use the compiler found by CMake, it is likely one that requires also
     # specifying the target sysroot to use. CMake's generator makes sure to pass --sysroot with
     # CMAKE_OSX_SYSROOT. Fortunately the compilers Apple ships also respect the SDKROOT environment
@@ -848,6 +870,7 @@ function(_add_cargo_build out_cargo_build_out_dir)
                 "${build_env_variable_genex}"
                 "${global_rustflags_genex}"
                 "${cargo_target_linker}"
+                "${cargo_host_target_linker}"
                 "${corrosion_cc_rs_flags}"
                 "${cargo_library_path}"
                 "CORROSION_BUILD_DIR=${CMAKE_CURRENT_BINARY_DIR}"
@@ -1234,8 +1257,31 @@ function(corrosion_link_libraries target_name)
         )
 
         if (TARGET "${library}")
+            # This works fine, except when compiling for ios. See https://cmake.org/pipermail/cmake/2016-March/063050.html
+            # XCODE_EMIT_EFFECTIVE_PLATFORM_NAME=OFF is supposed to prevent emitting EFFECTIVE_PLATFORM_NAME, but even
+            # with CMake 4.1 and the variable set to off EFFECTIVE_PLATFORM_NAME still leaks into generator expressions,
+            # and is not correctly replaced at build time
+            set(linker_dir "$<TARGET_LINKER_FILE_DIR:${library}>")
+            # Probably should also affect other apple OSs with a simulator
+            if(CMAKE_SYSTEM_NAME STREQUAL "iOS")
+                unset(platform_name)
+                message(CHECK_START "corrosion_link_libraries: Attempting to replace EFFECTIVE_PLATFORM_NAME")
+                if(CMAKE_OSX_SYSROOT MATCHES "iphoneos")
+                    set(platform_name "-iphoneos")
+                elseif(CMAKE_OSX_SYSROOT MATCHES "iphonesimulator")
+                    set(platform_name "-iphonesimulator")
+                else()
+                    # Todo: CMAKE_OSX_SYSROOT can be not set - how do we handle that?
+                    message(CHECK_FAIL "Failed to determine platform name for iOS target from sysroot ${CMAKE_OSX_SYSROOT}")
+                endif()
+                if(DEFINED platform_name)
+                    # This is a hack to fix $EFFECTIVE_PLATFORM_NAME not expanding in TARGET_LINKER_FILE_DIR
+                    set(linker_dir "$<PATH:REPLACE_FILENAME,${linker_dir},$<CONFIG>${platform_name}>")
+                    message(CHECK_PASS "done")
+                endif()
+            endif()
             corrosion_add_target_local_rustflags(${target_name}
-                "-L$<TARGET_LINKER_FILE_DIR:${library}>"
+                "-L${linker_dir}"
                 "-l$<TARGET_LINKER_FILE_BASE_NAME:${library}>"
             )
             add_dependencies(_cargo-build_${target_name} ${library})
@@ -2246,6 +2292,48 @@ function(corrosion_parse_package_version package_manifest_path out_package_versi
             "NOTFOUND"
             PARENT_SCOPE
         )
+    endif()
+endfunction()
+
+function(_corrosion_determine_host_compiler host_c_compiler_out error_out)
+    # Create minimal CMakeLists.txt to be generated for the host.
+    set(package_dir "${CMAKE_BINARY_DIR}/corrosion/host_compiler")
+    file(REMOVE_RECURSE "${package_dir}")
+    file(MAKE_DIRECTORY "${package_dir}")
+    set(lists "cmake_minimum_required(VERSION 3.10)\n")
+    string(APPEND lists "project(CorrosionDetermineHostCompiler LANGUAGES C)\n")
+    # We add a `:` after the compiler so we can easily match the end, since `:` is
+    # invalid in filenames.
+    string(APPEND lists "message(STATUS \"HOST_C_COMPILER=\${CMAKE_C_COMPILER}:\")\n")
+    file(WRITE "${package_dir}/CMakeLists.txt" "${lists}")
+
+    # Generate the CMake project.
+    execute_process(
+            COMMAND ${CMAKE_COMMAND}
+            -DCMAKE_CROSSCOMPILING=OFF
+            "${package_dir}"
+            WORKING_DIRECTORY "${package_dir}"
+            OUTPUT_VARIABLE host_detection_output
+            ERROR_VARIABLE host_detection_error
+            RESULT_VARIABLE host_detection_result
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            ERROR_STRIP_TRAILING_WHITESPACE
+    )
+
+    # Check if configuring the dummy CMake project failed.
+    if(NOT host_detection_result EQUAL 0)
+        message(WARNING "Failed to detect host compiler. Result: ${host_detection_result}\n"
+                "Output: ${host_detection_output}\n"
+                "Error: ${host_detection_result}")
+        return()
+    endif()
+
+    # Extract C compiler from the output.
+    string(REGEX MATCH "HOST_C_COMPILER=([^:\r\n]*):" host_c_compiler_match "${host_detection_output}")
+    if(host_c_compiler_match)
+        set("${host_c_compiler_out}" "${CMAKE_MATCH_1}" PARENT_SCOPE)
+    else()
+        set("${error_out}" "Regex match failure: ${host_detection_output}")
     endif()
 endfunction()
 
